d1d565eb6ed077a4a600081fbc0fcb63cec77877
kouji_kawamura <45913424+kouji0705@users.noreply.github.com>
2024-09-01 11:00:51 +0900

    fix: product cord

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/counter_provider.dart';
+import '../widgets/custom_button.dart'; // CustomButtonをインポート
 
 class HomeScreen extends StatelessWidget {
   @override
@@ -28,55 +29,17 @@ class HomeScreen extends StatelessWidget {
       floatingActionButton: Row(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
-          FloatingActionButton(
+          CustomButton(
+            label: 'Increment',
             onPressed: counterProvider.increment,
-            tooltip: 'Increment',
-            child: Icon(Icons.add),
           ),
           SizedBox(width: 10), // ボタン間のスペース
-          FloatingActionButton(
+          CustomButton(
+            label: 'Decrement',
             onPressed: counterProvider.decrement,
-            tooltip: 'Decrement',
-            child: Icon(Icons.remove),
           ),
         ],
       ),
     );
   }
 }
-
-// import 'package:flutter/material.dart';
-// import 'package:provider/provider.dart'; // providerをインポート
-// import '../providers/counter_provider.dart'; // CounterProviderをインポート
-
-// class HomeScreen extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     final counterProvider = Provider.of<CounterProvider>(context);
-
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Counter: ${counterProvider.counter}'),
-//       ),
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             Text(
-//               'You have pushed the button this many times:',
-//             ),
-//             Text(
-//               '${counterProvider.counter}',
-//               style: Theme.of(context).textTheme.headlineMedium,
-//             ),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: counterProvider.increment,
-//         tooltip: 'Increment',
-//         child: Icon(Icons.add),
-//       ),
-//     );
-//   }
-// }
